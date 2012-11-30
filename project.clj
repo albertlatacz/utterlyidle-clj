240396a98a071ecdf560ad34041f1ab18251918a
Albert Latacz <albert.latacz@gmail.com>
2012-11-30 08:51:45 +0000

    Added project info

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject utterlyidle-clj "1.0.0"
   :description "Clojure bindings for UtterlyIdle"
+  :url "https://github.com/albertlatacz/utterlyidle-clj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-path "src/clojure"
   :java-source-path "src/java"
   :repositories {"Dan Bodart" "http://repo.bodar.com.s3.amazonaws.com/"}
