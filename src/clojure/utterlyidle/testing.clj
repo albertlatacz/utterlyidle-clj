1516037e364a42e265a17f4d057f3da6678d8b8f
Albert Latacz <albert.latacz@db.com>
2014-01-24 16:22:55 +0000

    added testing-server macro

---
+++ src/clojure/utterlyidle/testing.clj
@@ -0,0 +1,12 @@
+(ns utterlyidle.testing
+  (:require [utterlyidle.bindings :refer :all]
+            [utterlyidle.server :refer :all]))
+
+(defmacro testing-server
+  "Creates new testing server context for given bindings and invokes body within it.
+  Use 'client' binding to query the server."
+  [bindings & body]
+  `(let [server# (start-server {} ~bindings)
+         ~'client (.application server#)]
+     (try ~@body
+          (finally (stop-server server#)))))
