d200ff942530f170d406227bf170cf2036e6c907
Albert Latacz <albert.latacz@gmail.com>
2014-01-31 21:12:47 +0000

    removed unnecessary nesting of source dirs

--- project.clj
+++ project.clj
@@ -5,8 +5,6 @@
   :url "https://github.com/albertlatacz/utterlyidle-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :source-paths ["src/clojure"]
-  :java-source-paths ["src/java"]
   :repositories {"Dan Bodart" "http://repo.bodar.com.s3.amazonaws.com/"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.namespace "0.2.4"]
